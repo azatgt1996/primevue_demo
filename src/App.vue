1cecaa95ae50fb874a533069107380b56483731f
azatgt1996 <azatgt1996@yandex.ru>
2024-03-21 19:35:24 +0300

    +primevue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import InputMask from 'primevue/inputmask';
+import MultiSelect from 'primevue/multiselect';
+import Textarea from 'primevue/textarea';
 
-const input = ref('')
+const form = ref({
+  name: '',
+  skills: [],
+  phone: '',
+  bio: '',
+})
+
+function onSubmit() {
+  alert(JSON.stringify(form.value, null, 2))
+}
 </script>
 
 <template>
-  {{ input || 'empty' }}<br>
-  <input v-model="input"/>
+  <form @submit.prevent="onSubmit" class="p-fluid m-auto w-max">
+    <div class="mb-3">
+      <label>Name</label>
+      <InputText v-model="form.name" required/>
+    </div>
+    <div class="mb-3">
+      <label>Phone</label>
+      <InputMask v-model="form.phone" mask="8 (999) 999-99-99" placeholder="8 (999) 999-99-99" />
+    </div>
+    <div class="mb-3">
+      <label>Bio</label>
+      <Textarea v-model="form.bio" autoResize/>
+    </div>
+    <div class="mb-3">
+      <label>Skills</label>
+      <MultiSelect v-model="form.skills" :options="['JS', 'C#']"/>
+    </div>
+
+    <Button label="Submit" type="submit"/>
+  </form>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 
-createApp(App).mount('#app')
+import './style.css'
+import 'primevue/resources/themes/aura-light-blue/theme.css'
+import 'primeflex/primeflex.css'
+
+createApp(App)
+  .use(PrimeVue)
+  .mount('#app')
